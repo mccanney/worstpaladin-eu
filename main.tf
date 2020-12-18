e8f6619e0de279ff3354c8fdfb3f38225edd4990
David McCanney <38220805+mccanney@users.noreply.github.com>
2020-12-18 12:27:17 +0000

    Update for terraform version
    - Updated provider configuration
    - Removed variable interpolation
    - Removed template provider and changed to templatefile function

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
-provider "aws" {
-    version = "~> 1.14"
-    region  = "${var.aws_region}"
-}
-
-provider "template" {
-    version = "~> 1.0"
-}
-
 terraform {
-    required_version = "~> 0.11"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+  }
+    required_version = ">= 0.13"
 
     backend "s3" {
         bucket  = "terraform-remote-state-bucket-s3"
@@ -18,12 +14,8 @@ terraform {
     }
 }
 
-data "template_file" "bucket_policy" {
-    template = "${file("policies/bucket-policy.json")}"
-
-    vars {
-      domain = "${var.domain}"
-    }
+provider "aws" {
+    region = var.aws_region
 }
 
 locals {
@@ -34,47 +26,47 @@ locals {
 }
 
 resource "aws_route53_zone" "zone" {
-    name              = "${var.domain}"
+    name              = var.domain
     comment           = "Route53 DNS zone for ${var.domain}"
-    delegation_set_id = "${var.delegation_set}"
+    delegation_set_id = var.delegation_set
 
-    tags {
-        site        = "${var.domain}"
+    tags = {
+        site        = var.domain
         environment = "production"
     }
 }
 
 resource "aws_s3_bucket" "web" {
-    bucket = "${var.domain}"
-    policy = "${data.template_file.bucket_policy.rendered}"
+    bucket = var.domain
+    policy = templatefile("policies/bucket-policy.json", { domain = var.domain })
 
     website {
-        index_document = "${local.html_files[0]}"
-        error_document = "${local.html_files[1]}"
+        index_document = local.html_files[0]
+        error_document = local.html_files[1]
     }
 
-    tags {
-        site        = "${var.domain}"
+    tags = {
+        site        = var.domain
         environment = "production"
     }
 }
 
 resource "aws_route53_record" "alias" {
-    zone_id = "${aws_route53_zone.zone.zone_id}"
-    name    = "${var.domain}"
+    zone_id = aws_route53_zone.zone.zone_id
+    name    = var.domain
     type    = "A"
 
     alias {
-        name                   = "${aws_s3_bucket.web.website_domain}"
-        zone_id                = "${aws_s3_bucket.web.hosted_zone_id}"
+        name                   = aws_s3_bucket.web.website_domain
+        zone_id                = aws_s3_bucket.web.hosted_zone_id
         evaluate_target_health = false
     }
 }
 
 resource "aws_s3_bucket_object" "files" {
-    count        = "${length(local.html_files)}"
-    bucket       = "${aws_s3_bucket.web.bucket}"
-    key          = "${local.html_files[count.index]}"
-    source       = "${format("html/%s", local.html_files[count.index])}"
+    count        = length(local.html_files)
+    bucket       = aws_s3_bucket.web.bucket
+    key          = local.html_files[count.index]
+    source       = format("html/%s", local.html_files[count.index])
     content_type = "text/html"
 }
