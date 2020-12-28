03a6649cff668479c73138cad23e275171ea4aeb
David McCanney <38220805+mccanney@users.noreply.github.com>
2020-12-28 15:39:12 +0000

    Add environment setup script
    - Added basic PowerShell environment setup script. Secret keys are handled
    secretly.
    
    - Moved the end-to-end tests out of the terratest directory.

---
+++ prod_env.ps1
@@ -0,0 +1,15 @@
+﻿$accessKey = Read-Host -Prompt 'Enter the production access key'
+$secretKey = Read-Host -Prompt 'Enter the production secret key' -AsSecureString
+
+$Env:AWS_ACCESS_KEY_ID = $accessKey
+$Env:AWS_SECRET_ACCESS_KEY = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secretKey)
+)
+$Env:PATH += ";C:\AWS"
+
+# https://github.com/gruntwork-io/terragrunt/issues/581
+$Env:TERRAGRUNT_DOWNLOAD = "C:\AWS"
+
+Set-Alias -Name terragrunt -Value C:\AWS\terragrunt_windows_amd64.exe
+
+Write-Host "You now have access to the PRODUCTION AWS environment.  Don't fuck things up.`n" -ForegroundColor Red
